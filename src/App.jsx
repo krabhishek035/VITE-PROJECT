07f87295fe7500145eadd4665f2ba0c4fb809bf8
Abhishek kumar <krabhishek035@gmail.com>
2023-07-01 11:20:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import CrewMembers from './CrewMembers'
+
+function App() {
+
+  return (
+    <>
+    <CrewMembers/>
+    </>
+  )
+}
+
+export default App
